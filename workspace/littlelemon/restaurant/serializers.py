685e17bdc69326274a7dada691b982ef5b7037b2
Luis garcia <luisegarcia9@hotmail.com>
2023-03-06 20:22:56 -0500

    api booking

---
+++ workspace/littlelemon/restaurant/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+from . import models
+
+class MenuSerializer(ModelSerializer):
+    class Meta:
+        model = models.Menu
+        fields = "__all__"
+        
+class BookingSerializer(ModelSerializer):
+    class Meta:
+        model = models.Booking
+        fields ="__all__"        
--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from . import models
+from . import serializers
+from rest_framework import generics, viewsets
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = models.Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = models.Menu.objects.all()
+    serializer_class = serializers.MenuSerializer 
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = models.Booking.objects.all()
+    serializer_class = serializers.BookingSerializer
+           
