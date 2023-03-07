33a63f37b92853eeeff4e8b8c3cf3a3a673f33b5
Luis garcia <luisegarcia9@hotmail.com>
2023-03-06 21:55:42 -0500

    secure booking endpoint

--- workspace/littlelemon/restaurant/urls.py
+++ workspace/littlelemon/restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     path('', views.index, name= 'index'),
     path('menu/',views.MenuItemView.as_view()),
-    path('menu/<int:pk>',views.SingleMenuItemView.as_view())
+    path('menu/<int:pk>',views.SingleMenuItemView.as_view()),
+    path('api-token-auth/', ObtainAuthToken.as_view())
 ]
--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from . import models
 from . import serializers
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 def index(request):
     return render(request, 'index.html', {})
@@ -16,6 +17,7 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     serializer_class = serializers.MenuSerializer 
     
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = models.Booking.objects.all()
     serializer_class = serializers.BookingSerializer
            
