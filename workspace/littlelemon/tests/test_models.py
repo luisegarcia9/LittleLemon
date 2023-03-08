9fa6032f7062e650eb7e512e560cf1cdb9884ce3
Luis garcia <luisegarcia9@hotmail.com>
2023-03-07 21:36:20 -0500

    test added

---
+++ workspace/littlelemon/tests/test_models.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from restaurant.models import Menu
+
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title = "icecream", price = 90, inventory = 20)
+        self.assertEqual(item, "icecream:90")
---
+++ workspace/littlelemon/tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase, Client
+from restaurant.models import Menu
+from restaurant.serializers import MenuSerializer
+
+
+class MenuViewTest(TestCase):
+    def setUp(self):
+        item1 = Menu.objects.create(title = "icecream", price = 90, inventory = 20)
+        item2 = Menu.objects.create(title = "postre", price = 10, inventory = 30)
+        
+    def test_getall(self):
+        items = Menu.objects.all()
+        data = MenuSerializer(items)
+        c = Client()
+        response = c.get('/restaurant/menu/menu/')
+        self.assertEqual(data,response.content)
+          
+        
